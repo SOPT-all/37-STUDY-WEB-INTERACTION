00c4052a25a76cbee9df5e207327c160b1cbb2b5
twossu <nadao202@naver.com>
2025-11-20 01:30:30 +0900

    8-4

--- yunha/week3/chapter8/8-1/01_dashboard.js
+++ yunha/week3/chapter8/8-1/01_dashboard.js
@@ -1,5 +1,22 @@
 import { get$, getAll$ } from "../utils.js";
 
+const setTheme = (theme = "light") => {
+  document.documentElement.setAttribute("class", theme);
+};
+
+const themeChangeHandler = (event) => {
+  const theme = document.documentElement.classList.contains("dark")
+    ? "light"
+    : "dark";
+  const $theme = event.target
+    .closest("button")
+    .querySelector(`[data-theme="${theme}"]`);
+
+  if ($theme) {
+    setTheme($theme.dataset.theme);
+  }
+};
+
 const openCommunity = () => {
   get$(".js-community").classList.add("active");
 };
@@ -12,6 +29,7 @@ document.addEventListener("DOMContentLoaded", () => {
   get$(".js-viewChange").addEventListener("click", viewChangeHandler);
   get$(".js-openCommunity").addEventListener("click", openCommunity);
   get$(".js-closeCommunity").addEventListener("click", closeCommunity);
+  get$(".js-theme").addEventListener("click", themeChangeHandler);
 });
 
 const viewChangeHandler = (event) => {
