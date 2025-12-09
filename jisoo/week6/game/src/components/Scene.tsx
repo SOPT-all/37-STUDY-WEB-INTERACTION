8d70420f348b19a1c9fa931b6da0fe51351d3ca8
임지수 <dlawltn0708@naver.com>
2025-12-09 14:00:51 +0900

    feat: TextureLoader로 바닥 생성

--- jisoo/week6/game/src/components/Scene.tsx
+++ jisoo/week6/game/src/components/Scene.tsx
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import * as THREE from "three";
 import Player from "./Player";
+import Floor from "./Floor";
 import Light from "./Light";
 import CameraController from "./CameraController";
 import { OrbitControls, OrthographicCamera } from "@react-three/drei";
@@ -8,6 +9,7 @@ import { OrbitControls, OrthographicCamera } from "@react-three/drei";
 const PLAYER_INITIAL_Y = 0.3;
 
 const Scene: React.FC = () => {
+  const floorRef = useRef<THREE.Mesh>(null);
   const [playerPosition, setPlayerPosition] = useState(
     new THREE.Vector3(0, PLAYER_INITIAL_Y, 0)
   );
@@ -16,6 +18,7 @@ const Scene: React.FC = () => {
     <>
       <Light />
       <OrthographicCamera makeDefault />
+      <Floor ref={floorRef} textureUrl="/grass.png" />
       <OrbitControls /> {/* 카메라 컨트롤러 */}
       <Player modelSrc="/manman.glb" />
       <CameraController playerPosition={playerPosition} />
